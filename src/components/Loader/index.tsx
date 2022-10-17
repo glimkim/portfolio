fbb96186874b8adf8f9e19fc5937970714011233
glimkim <limkimdev@gmail.com>
2022-10-17 18:06:06 +0100

    feat: create Loader component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,67 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+function Loader() {
+  return (
+    <LoadingContainer>
+      <ul>
+        <li className="letter">L</li>
+        <li className="letter">I</li>
+        <li className="letter">M</li>
+        <li className="letter">K</li>
+        <li className="letter">I</li>
+        <li className="letter">M</li>
+      </ul>
+    </LoadingContainer>
+  );
+}
+
+const LoadingContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background-color: ${({ theme: { colors } }) => `${colors.keyColor}`};
+  ul {
+    display: flex;
+    width: fit-content;
+    li.letter {
+      display: block;
+      font-size: 4rem;
+      animation-name: Letter;
+      animation-duration: 3s;
+      animation-iteration-count: infinite;
+      animation-timing-function: ease-in-out;
+      ${() => {
+        let styles = '';
+        for (let i = 1; i <= 6; i += 1) {
+          styles += `&:nth-type(${i}){
+                animation-delay: ${i * 200}ms;
+            }`;
+        }
+        return css`
+          ${styles}
+        `;
+      }};
+    }
+  }
+
+  @keyframes Letter {
+    0% {
+      transform: scale(0, 0);
+    }
+    50% {
+      transform: scale(1, 1);
+    }
+    100% {
+      transform: scale(0, 0);
+    }
+  }
+`;
+
+export default Loader;

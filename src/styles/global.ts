12b8adeb2511a8fac6da26c6c1ef05ad688c6c2d
glimkim <limkimdev@gmail.com>
2022-10-19 18:36:54 +0100

    refactor: update global.ts to use theme in global css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react';
+import { css, Theme } from '@emotion/react';
 import { resetCss } from './reset';
 
 export const pageWidth = '1440px';
@@ -6,36 +6,39 @@ export const headerHeight = '60px';
 export const drawerWidthClosed = '70px';
 export const drawerWidthOpen = '240px';
 
-export const globalStyles = css`
-  ${resetCss};
-  @font-face {
-    font-family: 'BlackHanSans';
-    font-style: normal;
-    font-weight: 400;
-    font-display: fallback;
-    src: url(./fonts/BlackHanSans-Regular.woff2) format('woff2'),
-      url(./fonts/BlackHanSans-Regular.woff) format('woff'),
-      url(./fonts/BlackHanSans-Regular.ttf) format('truetype');
-  }
+export const getGlobalStyles = (theme: Theme) => {
+  return css`
+    ${resetCss};
+    @font-face {
+      font-family: 'BlackHanSans';
+      font-style: normal;
+      font-weight: 400;
+      font-display: fallback;
+      src: url(./fonts/BlackHanSans-Regular.woff2) format('woff2'),
+        url(./fonts/BlackHanSans-Regular.woff) format('woff'),
+        url(./fonts/BlackHanSans-Regular.ttf) format('truetype');
+    }
 
-  * {
-    font-family: 'BlackHanSans', sans-serif;
-    font-size: 1rem;
-  }
+    * {
+      font-family: 'IBM Plex Mono', sans-serif;
+      font-size: 1rem;
+      color: ${theme.colors.font};
+    }
 
-  @media screen and (min-width: 1440px) {
-    html {
-      font-size: 18px;
+    @media screen and (min-width: 1440px) {
+      html {
+        font-size: 18px;
+      }
     }
-  }
-  @media screen and (max-width: 1080px) {
-    html {
-      font-size: 16px;
+    @media screen and (max-width: 1080px) {
+      html {
+        font-size: 16px;
+      }
     }
-  }
-  @media screen and (max-width: 767px) {
-    html {
-      font-size: 14px;
+    @media screen and (max-width: 767px) {
+      html {
+        font-size: 14px;
+      }
     }
-  }
-`;
+  `;
+};

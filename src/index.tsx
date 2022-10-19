94eed8bea4ff486ab8c6dde53e85305de4ad2e48
glimkim <limkimdev@gmail.com>
2022-10-19 18:33:59 +0100

    refactor: move PageThemeProvider to index.tsx
    To decide theme depending on PageThemeContext, I had to wrap App.tsx with PageThemeProvider.

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import PageThemeProvider from 'context/pageTheme';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <PageThemeProvider>
+      <App />
+    </PageThemeProvider>
   </React.StrictMode>,
 );
 

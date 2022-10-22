6d2809377a8600bd53120c5d0302cbeaa4836ac4
glimkim <limkimdev@gmail.com>
2022-10-22 17:10:06 +0100

    chore: change logo assets to png files

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,22 +1,26 @@
-import React, { HTMLAttributes, useContext } from 'react';
+import React, { useContext } from 'react';
 import styled from '@emotion/styled';
-import { ReactComponent as DefaultLogo } from 'assets/images/logo_default.svg';
-import { ReactComponent as DarkLogo } from 'assets/images/logo_dark.svg';
-import { PageTheme, PageThemeContext } from 'context/pageTheme';
+import DefaultLogo from 'assets/images/default_logo.png';
+import DarkLogo from 'assets/images/dark_logo.png';
+import { PageThemeContext } from 'context/pageTheme';
 
 function Logo() {
   const pageTheme = useContext(PageThemeContext);
 
   return (
     <StyledLogo>
-      {pageTheme === 'default' ? <DefaultLogo /> : <DarkLogo />}
+      {pageTheme === 'default' ? (
+        <img src={DefaultLogo} alt="logo" />
+      ) : (
+        <img src={DarkLogo} alt="logo" />
+      )}
     </StyledLogo>
   );
 }
 
 const StyledLogo = styled.h1`
   height: 32px;
-  svg {
+  img {
     height: 100%;
   }
 `;

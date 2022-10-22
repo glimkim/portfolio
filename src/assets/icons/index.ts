d4f59d46ac604a83f4818f8d0bad0d53bdbd8301
glimkim <limkimdev@gmail.com>
2022-10-22 17:05:05 +0100

    feat: create SVG component for icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+export { ReactComponent as LogoDefault } from './logo_default.svg';
+export { ReactComponent as LogoDark } from './logo_dark.svg';
+export { ReactComponent as Arrow } from './arrow_icon.svg';
+export { ReactComponent as GitHub } from './gitHub_icon.svg';
+export { ReactComponent as LinkedIn } from './linkedIn_icon.svg';
+export { ReactComponent as Notion } from './notion_icon.svg';
+export { ReactComponent as Mode } from './mode_icon.svg';
---
+++ src/components/SVG/index.tsx
@@ -0,0 +1,40 @@
+import React, { HTMLAttributes, useContext } from 'react';
+import * as icons from 'assets/icons';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+type IconType = keyof typeof icons;
+
+interface SVGProps extends HTMLAttributes<SVGSVGElement> {
+  icon: IconType;
+}
+
+function SVG({ icon }: SVGProps) {
+  const SVGImg: React.FunctionComponent<React.SVGProps<SVGSVGElement>> =
+    icons[icon];
+
+  return (
+    <Img icon={icon}>
+      <SVGImg />
+    </Img>
+  );
+}
+
+const Img = styled.figure<{ icon: IconType }>`
+  ${({ theme: { colors }, icon }) => {
+    const iconsToColor: IconType[] = ['GitHub', 'Notion', 'LinkedIn', 'Mode'];
+    let styles = ``;
+    if (iconsToColor.includes(icon)) {
+      styles += `svg {
+          path {
+            fill: ${colors.font};
+          }
+        }`;
+    }
+    return css`
+      ${styles}
+    `;
+  }}
+`;
+
+export default SVG;

1e0ade205de6863acd45322f6f943e3accd22cfe
glimkim <limkimdev@gmail.com>
2022-10-24 13:04:28 +0100

    feat: create Button component

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+import React, { HTMLAttributes } from 'react';
+
+type ButtonTheme = 'default' | 'sub';
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  theme?: ButtonTheme;
+  children: React.ReactNode;
+}
+
+function Button({ theme = 'default', children, ...props }: ButtonProps) {
+  return (
+    <StyledButton buttonTheme={theme} type="button" {...props}>
+      {children}
+    </StyledButton>
+  );
+}
+
+const StyledButton = styled.button<{ buttonTheme: ButtonTheme }>`
+  position: relative;
+  z-index: 10;
+  padding: 0.5rem 1rem;
+  border: 1px solid ${({ theme: { colors } }) => colors.border};
+  background-color: ${({
+    theme: {
+      colors: { button },
+    },
+    buttonTheme,
+  }) => (buttonTheme === 'default' ? button.default : button.sub)};
+  box-shadow: 6px 5px ${({ theme: { colors } }) => colors.button.shadow};
+  color: ${({ theme: { colors } }) => colors.button.font};
+  transition: 0.3s;
+  &:hover {
+    background-color: ${({ theme: { colors } }) => colors.pointColor};
+  }
+`;
+
+export default Button;

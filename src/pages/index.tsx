a765a834f96f6cdd47cfc3a6020e0dfbf7734d14
glimkim <limkimdev@gmail.com>
2022-10-26 14:27:45 +0100

    feat: add elements for the complete view and save mode in local storage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import WebFont from 'webfontloader';
-import Loader from 'components/Loader';
+import Loader from 'components/common/Loader';
 import PageTemplate from 'layout/PageTemplate';
 import { headerHeight } from 'styles/global';
+import Main from 'components/section/Main';
+import AboutMe from 'components/section/AboutMe';
+import Works from 'components/section/Works';
+import Contact from 'components/section/Contact';
+import {
+  PageTheme,
+  PageThemeContext,
+  PageThemeDispatch,
+} from 'context/pageTheme';
 
 function Home() {
+  const pageTheme = useContext(PageThemeContext);
+  const pageThemeDispatch = useContext(PageThemeDispatch);
   const [fontLoaded, setFontLoaded] = useState<boolean>(false);
   WebFont.load({
     google: {
@@ -21,21 +32,52 @@ function Home() {
     },
   });
 
+  useEffect(() => {
+    const modeFromStorage = localStorage.getItem('mode');
+
+    if (modeFromStorage && modeFromStorage !== pageTheme && pageThemeDispatch) {
+      pageThemeDispatch({
+        type: 'set',
+        newTheme: modeFromStorage as PageTheme,
+      });
+    }
+  }, [pageTheme, pageThemeDispatch, localStorage.getItem('mode')]);
+
   if (!fontLoaded) return <Loader />;
   return (
     <PageTemplate>
-      <PageContainer>contents</PageContainer>
+      <PageContainer>
+        <Main />
+        <div className="sections">
+          <AboutMe />
+          <Works />
+          <Contact />
+        </div>
+      </PageContainer>
     </PageTemplate>
   );
 }
 
 const PageContainer = styled.div`
-  padding-left: 2rem;
   border-right: 1px solid ${({ theme: { colors } }) => colors.border};
   min-height: calc(100vh - ${headerHeight});
-  *,
-  * > * {
-    color: ${({ theme: { colors } }) => colors.font} !important;
+
+  & > div.sections {
+    padding-left: 4rem;
+  }
+
+  @media screen and (max-width: 1440px) {
+    border: none;
+  }
+  @media screen and (max-width: 1080px) {
+    & > div.sections {
+      padding-left: 3rem;
+    }
+  }
+  @media screen and (max-width: 767px) {
+    & > div.sections {
+      padding: 0;
+    }
   }
 `;
 

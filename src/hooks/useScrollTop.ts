0ef733af7ddd560b5ec008633c64164bd2932829
glimkim <limkimdev@gmail.com>
2022-10-31 17:00:57 +0000

    feat: create useScrollTop hook
    to prevent stopping too many window scroll event triggers, used throttle method from lodash

---
+++ src/hooks/useScrollTop.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect, useMemo } from 'react';
+import _ from 'lodash';
+
+function useScrollTop() {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const onScroll = _.throttle(() => {
+    setScrollTop(window.scrollY);
+  }, 400);
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
+  return {
+    scrollTop,
+  };
+}
+
+export default useScrollTop;

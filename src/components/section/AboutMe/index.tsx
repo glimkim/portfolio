1f54cfaa1a49e5a0489da0a936c76f2eba1a0d3e
glimkim <limkimdev@gmail.com>
2022-11-01 18:20:19 +0000

    feat: complete about me section view

--- src/components/section/AboutMe/index.tsx
+++ src/components/section/AboutMe/index.tsx
@@ -2,6 +2,22 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import ProfileImg from 'components/common/ProfileImg';
 import SectionTitle from 'components/common/SectionTitle';
+import Proficient01 from 'assets/images/skill_icon_proficient_01.png';
+import Proficient02 from 'assets/images/skill_icon_proficient_02.png';
+import Proficient03 from 'assets/images/skill_icon_proficient_03.png';
+import Proficient04 from 'assets/images/skill_icon_proficient_04.png';
+import Proficient05 from 'assets/images/skill_icon_proficient_05.png';
+import Proficient06 from 'assets/images/skill_icon_proficient_06.png';
+import Tried01 from 'assets/images/skill_icon_tried_01.png';
+import Tried02 from 'assets/images/skill_icon_tried_02.png';
+import Tried03 from 'assets/images/skill_icon_tried_03.png';
+import Tried04 from 'assets/images/skill_icon_tried_04.png';
+import Tools01 from 'assets/images/skill_icon_tools_01.png';
+import Tools02 from 'assets/images/skill_icon_tools_02.png';
+import Tools03 from 'assets/images/skill_icon_tools_03.png';
+import Tools04 from 'assets/images/skill_icon_tools_04.png';
+import Tools05 from 'assets/images/skill_icon_tools_05.png';
+import Tools06 from 'assets/images/skill_icon_tools_06.png';
 
 function AboutMe() {
   return (
@@ -30,32 +46,80 @@ function AboutMe() {
           <div>
             <h6>familiar with</h6>
             <ul>
-              <li>ss</li>
-              <li>ss</li>
-              <li>ss</li>
-              <li>ss</li>
-              <li>ss</li>
+              <li>
+                <img src={Proficient01} alt="js_icon" />
+                JavaScript
+              </li>
+              <li>
+                <img src={Proficient02} alt="ts_icon" />
+                TypeScript
+              </li>
+              <li>
+                <img src={Proficient03} alt="react_icon" />
+                React JS
+              </li>
+              <li>
+                <img src={Proficient04} alt="html_icon" />
+                HTML
+              </li>
+              <li>
+                <img src={Proficient05} alt="css_icon" />
+                CSS
+              </li>
+              <li>
+                <img src={Proficient06} alt="scss_icon" />
+                SCSS
+              </li>
             </ul>
           </div>
           <div>
             <h6>experienced</h6>
             <ul>
-              <li>ss</li>
-              <li>ss</li>
-              <li>ss</li>
-              <li>ss</li>
-              <li>ss</li>
-              <li>ss</li>
+              <li>
+                <img src={Tried01} alt="php_icon" />
+                PHP
+              </li>
+              <li>
+                <img src={Tried02} alt="mysql_icon" />
+                MySQL
+              </li>
+              <li>
+                <img src={Tried03} alt="nodejs_icon" />
+                NodeJS
+              </li>
+              <li>
+                <img src={Tried04} alt="firebase_icon" />
+                Firebase
+              </li>
             </ul>
           </div>
           <div>
             <h6>tools</h6>
             <ul>
-              <li>ss</li>
-              <li>ss</li>
-              <li>ss</li>
-              <li>ss</li>
-              <li>ss</li>
+              <li>
+                <img src={Tools01} alt="gitHub_icon" />
+                gitHub
+              </li>
+              <li>
+                <img src={Tools02} alt="zeplin_icon" />
+                Zeplin
+              </li>
+              <li>
+                <img src={Tools03} alt="figma_icon" />
+                Figma
+              </li>
+              <li>
+                <img src={Tools04} alt="xd_icon" />
+                Adobe XD
+              </li>
+              <li>
+                <img src={Tools05} alt="illustrator_icon" />
+                Adobe Illustrator
+              </li>
+              <li>
+                <img src={Tools06} alt="photoshop_icon" />
+                Adobe Photoshop
+              </li>
             </ul>
           </div>
         </StrengthBox>
@@ -88,6 +152,28 @@ const StrengthBox = styled.ul`
     `;
   }};
 
+  li {
+    display: flex;
+    align-items: center;
+    gap: 0.7rem;
+    padding: 0.5rem 0;
+    font-weight: bold;
+    transition: 0.6s;
+    transform-origin: left center;
+    &:first-of-type {
+      padding-top: 1rem;
+    }
+    &:last-of-type {
+      padding-bottom: 0;
+    }
+    &:hover {
+      transform: scale(1.1, 1.1);
+    }
+    img {
+      width: 2rem;
+    }
+  }
+
   @media screen and (max-width: 1080px) {
     flex-direction: column;
     align-items: center;
@@ -117,6 +203,7 @@ const SectionWrapper = styled.div`
     h5 {
       font-family: 'BlackHanSans';
       font-size: 2rem;
+      color: ${({ theme: { colors } }) => colors.title};
     }
     h6 {
       font-family: 'BlackHanSans';

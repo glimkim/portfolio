d305626aca0229532392251a22698059fceddcde
glimkim <limkimdev@gmail.com>
2022-12-05 17:36:28 +0000

    style: edit object position

--- src/components/common/ProfileImg/index.tsx
+++ src/components/common/ProfileImg/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import profilePic from 'assets/images/profile_img.png';
+import profilePic from 'assets/images/profile_img.jpg';
 
 type ProfileImgType = 'square' | 'circle';
 
